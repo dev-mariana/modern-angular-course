c1275a43e643cc0d738bba0452ed370390054be9
Mariana Bastos <m.b.rodrigues1998@gmail.com>
2026-06-06 20:50:50 -0300

    first standalone component and compoenent templates & interaction

---
+++ src/app/hello/hello.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  imports: [],
+  templateUrl: './hello.html',
+  styleUrl: './hello.scss',
+})
+export class Hello {
+  protected  title = 'Welcome to Modern Angular!';
+
+  protected isDisabled = false;
+
+  onClick(): void {
+    console.log('Button clicked!');
+    this.isDisabled = !this.isDisabled;
+  }
+}
